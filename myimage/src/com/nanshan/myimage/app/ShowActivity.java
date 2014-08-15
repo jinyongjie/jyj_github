a56e9d20c35cec62e72dcbfd053dd9830fdf31a2
jinyongjie <jinyongjie@360.cn>
2014-08-15 19:51:29 +0800

    ShowActivity.java

--- myimage/src/com/nanshan/myimage/app/ShowActivity.java
+++ myimage/src/com/nanshan/myimage/app/ShowActivity.java
@@ -6,12 +6,21 @@ import java.util.ArrayList;
 import com.nanshan.myimage.R;
 import com.nanshan.myimage.R.layout;
 import com.nanshan.myimage.R.menu;
+import com.nanshan.myimage.data.ImageLoader;
+import com.nanshan.myimage.data.ImageMgr;
+import com.nanshan.myimage.data.ImageLoader.ImageCallback;
+import com.nanshan.myimage.data.ImageMgr.ImageInfo;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageSwitcher;
@@ -45,8 +54,8 @@ public class ShowActivity extends Activity {
 				ImageView imageView = new ImageView(ShowActivity.this);
 				imageView.setBackgroundColor(0xff0000);
 				imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-				imageView.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.WRAP_CONTENT,
-						LayoutParams.WRAP_CONTENT));
+				imageView.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.MATCH_PARENT,
+						LayoutParams.MATCH_PARENT));
 				return imageView;
 			}
 		});
@@ -55,6 +64,17 @@ public class ShowActivity extends Activity {
 				android.R.anim.fade_in));
 		mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,
 				android.R.anim.fade_out));
+		mSwitcher.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				ShowActivity.this.finish();
+			}
+			
+		});
+		ShowNext(0);
 	}
 
 	@Override
@@ -63,4 +83,47 @@ public class ShowActivity extends Activity {
 		getMenuInflater().inflate(R.menu.show, menu);
 		return true;
 	}
+
+	void ShowNext(int time) {
+		Handler handler = new Handler();
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				// 要做的事情
+				if(mIndex == mArray.size())
+				{
+					ShowActivity.this.finish();
+				}
+				else
+				{
+					DisplayMetrics dm = new DisplayMetrics();
+					getWindowManager().getDefaultDisplay().getMetrics(dm);
+					
+					ImageInfo info = ImageMgr.GetInstance().GetImage(mArray.get(mIndex));
+					ImageLoader.GetInstance().GetImage(info.id, info.path, dm.widthPixels, dm.heightPixels, false, callback);
+
+				}
+			}
+		};
+		handler.postDelayed(runnable, time);//
+	}
+	ImageCallback callback = new ImageCallback()
+	{
+
+		@Override
+		public void OnImageLoad(Bitmap bitmap, String path) {
+			// TODO Auto-generated method stub
+			mSwitcher.setImageDrawable(new BitmapDrawable(bitmap));
+			mIndex++;
+			ShowNext(2000);
+		}
+
+		@Override
+		public void OnImageRotate(int id) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
 }
